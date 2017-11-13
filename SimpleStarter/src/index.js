a7f42f2711610d85576f0bad4c5ce599eac5580d
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-13 17:53:01 -0600

    Add React boiler plate

---
+++ SimpleStarter/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>Test</div>
+  )
+};
+
+ReactDOM.render(<App />, document.querySelector('.container'));
