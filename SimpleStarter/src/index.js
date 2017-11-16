762e7f6f6fa448ccd5b57dc8e3d5782921b639b9
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-16 12:02:03 -0600

    Create input for Search Bar and import from default index.js

--- SimpleStarter/src/index.js
+++ SimpleStarter/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDfbfG7MleyONDs4jstjIx61F-lqIkU5mU';
 
 const App = () => {
   return (
-    <div>Test</div>
+    <div>
+      <SearchBar />
+    </div>
   )
 };
 
