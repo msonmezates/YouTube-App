1758033fe9f4b3e7dc447fe03a210dd29a197564
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-16 15:28:03 -0600

    Create state for Search Bar component

--- SimpleStarter/src/components/search_bar.js
+++ SimpleStarter/src/components/search_bar.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: ''
+    };
+  }
   render() {
     return(
       <input onChange={e => console.log(e.target.value)} />
