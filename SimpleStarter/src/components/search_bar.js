f9a90c944f9e87b592f0df65e31bbbb4c6b53a3a
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-16 15:48:29 -0600

    Remove string from state and leave it empty

--- SimpleStarter/src/components/search_bar.js
+++ SimpleStarter/src/components/search_bar.js
@@ -4,7 +4,7 @@ export default class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchTerm: 'lo'
+      searchTerm: ''
     };
   }
   render() {
