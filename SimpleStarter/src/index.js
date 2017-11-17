39821f1433e23d4b06bc49ce8a1b906baf4e0d8e
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-17 15:41:27 -0600

    Import youtube search api and test by searching a term

--- SimpleStarter/src/index.js
+++ SimpleStarter/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDfbfG7MleyONDs4jstjIx61F-lqIkU5mU';
+
+YTSearch({ key: API_KEY, term: 'surfboards' }, data => {
+  console.log(data);
+});
 
 const App = () => {
   return (
