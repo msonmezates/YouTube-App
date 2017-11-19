0198c7c4eefdcd882e325ec37a2c53963857fba9
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-18 23:18:39 -0600

    Build a Video Item List with map method

--- SimpleStarter/src/components/video_list.js
+++ SimpleStarter/src/components/video_list.js
@@ -3,9 +3,12 @@ import PropTypes from 'prop-types';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+
+  const videoItems = props.videos.map(video => <VideoListItem video={video} />)
+
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {videoItems}
     </ul>
   );
 }
