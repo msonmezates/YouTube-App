e74d926ce992f81903ce4507ecb4343e629164d8
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-18 23:50:15 -0600

    Create boiler plate of VideoDetail component

--- SimpleStarter/src/index.js
+++ SimpleStarter/src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDfbfG7MleyONDs4jstjIx61F-lqIkU5mU';
@@ -22,6 +23,7 @@ class App extends Component {
       <div>
         <SearchBar />
         <VideoList videos={this.state.videos} />
+        <VideoDetail />
       </div>
     );
   }
