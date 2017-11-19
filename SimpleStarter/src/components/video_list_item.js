b146b3fd32ae3e54fb4ab6830578e2928147d48f
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-18 23:29:11 -0600

    Add prop types for video object

--- SimpleStarter/src/components/video_list_item.js
+++ SimpleStarter/src/components/video_list_item.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const VideoListItem = props => {
+const VideoListItem = ({video}) => {
+  console.log(video);
   return (
     <li>Video Item</li>
   );
 }
+
+VideoListItem.propTypes = {
+  video: PropTypes.object
+};
 
 export default VideoListItem;
