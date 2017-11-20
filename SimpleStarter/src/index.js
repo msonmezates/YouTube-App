0ba42e502f399197741dd28bd154211c32bb6b70
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-19 20:09:44 -0600

    Throttle search term input and apply debunced function

--- SimpleStarter/src/index.js
+++ SimpleStarter/src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/search_bar";
@@ -26,9 +27,12 @@ class App extends Component {
     });
   }
   render() {
+    //debounce is used to delay invoking the function in milliseconds 
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 500);
+
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
